66bbfdee5b580e8e86d0150768cf420791cf84ce
BitsAndBarks <142544882+BitsAndBarks@users.noreply.github.com>
2023-10-30 00:41:04 +0100

    updated task 2 with comma-seperated string concatenation where possible (if no punctuation mark follows)

--- Day02/input-output.py
+++ Day02/input-output.py
@@ -4,14 +4,14 @@ Let's get to know each other :-)
 """)
 
 userName = input("First, please tell me your name. ")
-print("Nice to meet you, " + userName + "!")
+print("Nice to meet you,", userName + "!")
 print()
 userFood = input("What is your favourite food? ")
-print("What a coincidence - I also like " + userFood + "!")
+print("What a coincidence - I also like", userFood + "!")
 print()
 userMusic = input("And what kind of music do you like? ")
 print("""That's not really my cup of tea, but you do you, right? :-)
 
-Anyway... I have to go. You made me crave """ + userFood + """ really badly, so I'm going out to buy some.
+Anyway... I have to go. You made me crave""", userFood, """really badly, so I'm going out to buy some.
       
         Have a nice day, bye!""")
