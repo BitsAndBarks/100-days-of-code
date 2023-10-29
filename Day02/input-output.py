e61b6b8f184c79ef84dbf90388de0a7069e9c331
BitsAndBarks <142544882+BitsAndBarks@users.noreply.github.com>
2023-10-30 00:29:33 +0100

    Day 2 task complete

---
+++ Day02/input-output.py
@@ -0,0 +1,17 @@
+print("""Hi there!
+
+Let's get to know each other :-)
+""")
+
+userName = input("First, please tell me your name. ")
+print("Nice to meet you, " + userName + "!")
+print()
+userFood = input("What is your favourite food? ")
+print("What a coincidence - I also like " + userFood + "!")
+print()
+userMusic = input("And what kind of music do you like? ")
+print("""That's not really my cup of tea, but you do you, right? :-)
+
+Anyway... I have to go. You made me crave """ + userFood + """ really badly, so I'm going out to buy some.
+      
+        Have a nice day, bye!""")
