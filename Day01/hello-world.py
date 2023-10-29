1dacbe0a16353a129ea439b17bea779cee4a6cbc
BitsAndBarks <142544882+BitsAndBarks@users.noreply.github.com>
2023-10-30 00:14:15 +0100

    Day 1 task complete

---
+++ Day01/hello-world.py
@@ -0,0 +1,13 @@
+print("""
+Hello World!
+I'm committing to a 100 days of code challenge to force myself to more consistency.
+I'm not a total beginner so I'm hoping to get through the first two or three weeks fairly easily.
+My aim is to make it a habit to code more often in my free time when I don't have to do assignments.
+Therefore, all programs and code snippets I'll be contributing into the 100 days of code challenge 
+won't be part of any university-related compulsory tasks.
+
+I'm not following one exact challenge roadmap because I found that my personal goals somewhat differ 
+from any of the premade challenges that are out there. Instead I've looked through multiple resources 
+and made my own plan. 
+
+""")
